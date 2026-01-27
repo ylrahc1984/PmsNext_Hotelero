a969acacb73a71a6d654f9c6488e3933e57a2d7f
charly <coleccionista84@gmail.com>
2026-01-27 15:38:33 -0600

    chore(prod): actualizar apiUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ import packageInfo from '../../package.json';
 export const environment = {
   appVersion: packageInfo.version,
   production: true,
-  apiUrl: 'http://144.126.133.28/api',
+  apiUrl: 'https://api.pmsnextdevcloud.com/api',
   googleMapsApiKey: 'AIzaSyA-o3tTaSdRzt7JBBjRwBzYGNpUEtGyfF8'
 };
